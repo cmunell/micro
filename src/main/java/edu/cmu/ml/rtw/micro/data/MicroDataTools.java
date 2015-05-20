1c326925eea7de7d4233312266409da3a085abb8
Ndapa Nakashole <ndapa.nakashole@gmail.com>
2015-05-20 12:21:09 -0400

    Add prepositional phrase attachment microreader

--- src/main/java/edu/cmu/ml/rtw/micro/data/MicroDataTools.java
+++ src/main/java/edu/cmu/ml/rtw/micro/data/MicroDataTools.java
@@ -3,18 +3,21 @@ package edu.cmu.ml.rtw.micro.data;
 import edu.cmu.ml.rtw.generic.data.DataTools;
 import edu.cmu.ml.rtw.generic.util.OutputWriter;
 import edu.cmu.ml.rtw.micro.cat.data.annotation.nlp.AnnotationTypeNLPCat;
+import edu.cmu.ml.rtw.ppa.predict.PPADisambiguator;
 import edu.cmu.ml.rtw.AnnotationVerb;
 
 public class MicroDataTools extends DataTools {
-	public MicroDataTools() {
-		this(new OutputWriter());
-	}
-	
-	public MicroDataTools(OutputWriter outputWriter) {
-		super(outputWriter);		
-		
-		/* Add micro-reading annotation types here */
-		this.addAnnotationTypeNLP(AnnotationTypeNLPCat.NELL_CATEGORY);
-		this.addAnnotationTypeNLP(AnnotationVerb.NELL_VERB);
-	}
+
+  public MicroDataTools() {
+    this(new OutputWriter());
+  }
+
+  public MicroDataTools(OutputWriter outputWriter) {
+    super(outputWriter);
+
+    /* Add micro-reading annotation types here */
+    this.addAnnotationTypeNLP(AnnotationTypeNLPCat.NELL_CATEGORY);
+    this.addAnnotationTypeNLP(AnnotationVerb.NELL_VERB);
+    this.addAnnotationTypeNLP(PPADisambiguator.PPA_DISAMBIG);
+  }
 }
