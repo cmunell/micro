cb00f9d213520ef645b436ae018ffd021afa58ce
Jayant Krishnamurthy <me@jayantkrish.com>
2015-05-19 20:56:34 -0400

    adding semantic parser annotator to pipeline

--- src/main/java/edu/cmu/ml/rtw/micro/model/annotation/nlp/PipelineNLPMicro.java
+++ src/main/java/edu/cmu/ml/rtw/micro/model/annotation/nlp/PipelineNLPMicro.java
@@ -2,6 +2,7 @@ package edu.cmu.ml.rtw.micro.model.annotation.nlp;
 
 import edu.cmu.ml.rtw.generic.model.annotator.nlp.PipelineNLP;
 import edu.cmu.ml.rtw.micro.cat.data.annotation.nlp.NELLMentionCategorizer;
+import edu.cmu.ml.rtw.micro.model.annotator.semparse.SemparseAnnotatorSentence;
 
 public class PipelineNLPMicro extends PipelineNLP {
 	public PipelineNLPMicro() {
@@ -11,11 +12,12 @@ public class PipelineNLPMicro extends PipelineNLP {
 		 * Initialize micro-readers here
 		 */
 		NELLMentionCategorizer mentionCategorizer = new NELLMentionCategorizer();
-		
-		
+    SemparseAnnotatorSentence semanticParser = SemparseAnnotatorSentence.fromSerializedModels("src/main/resources/parser.ser", "src/main/resources/supertagger.ser");
+
 		/*
 		 * Add micro-readers to the pipeline here
 		 */
 		addAnnotator(mentionCategorizer.produces(), mentionCategorizer);
+		addAnnotator(semanticParser.produces(), semanticParser);
 	}
 }
