94a36c523275811fc66b5d0006aee9667b95079e
Ndapa Nakashole <ndapa.nakashole@gmail.com>
2015-05-20 12:37:32 -0400

    Add micro-ppa to pom

--- src/main/java/edu/cmu/ml/rtw/micro/model/annotation/nlp/PipelineNLPMicro.java
+++ src/main/java/edu/cmu/ml/rtw/micro/model/annotation/nlp/PipelineNLPMicro.java
@@ -2,7 +2,7 @@ package edu.cmu.ml.rtw.micro.model.annotation.nlp;
 
 import edu.cmu.ml.rtw.generic.model.annotator.nlp.PipelineNLP;
 import edu.cmu.ml.rtw.micro.cat.data.annotation.nlp.NELLMentionCategorizer;
-<<<<<<< HEAD
+import edu.cmu.ml.rtw.micro.model.annotator.semparse.SemparseAnnotatorSentence;
 import edu.cmu.ml.rtw.ppa.predict.PPADisambiguator;
 import edu.cmu.ml.rtw.AnnotationVerb;
 
@@ -15,6 +15,7 @@ public class PipelineNLPMicro extends PipelineNLP {
      * Initialize micro-readers here
      */
     NELLMentionCategorizer mentionCategorizer = new NELLMentionCategorizer();
+    SemparseAnnotatorSentence semanticParser = SemparseAnnotatorSentence.fromSerializedModels("src/main/resources/parser.ser", "src/main/resources/supertagger.ser");
     AnnotationVerb annotationVerb = new AnnotationVerb();
     PPADisambiguator ppa = new PPADisambiguator();
 
@@ -22,30 +23,10 @@ public class PipelineNLPMicro extends PipelineNLP {
      * Add micro-readers to the pipeline here
      */
     addAnnotator(mentionCategorizer.produces(), mentionCategorizer);
+    addAnnotator(semanticParser.produces(), semanticParser);
     addAnnotator(annotationVerb.produces(), annotationVerb);
     addAnnotator(ppa.produces(), ppa);
   }
-=======
-import edu.cmu.ml.rtw.micro.model.annotator.semparse.SemparseAnnotatorSentence;
-import edu.cmu.ml.rtw.AnnotationVerb;
-
-public class PipelineNLPMicro extends PipelineNLP {
-	public PipelineNLPMicro() {
-		super();
-		
-		/*
-		 * Initialize micro-readers here
-		 */
-		NELLMentionCategorizer mentionCategorizer = new NELLMentionCategorizer();
-    SemparseAnnotatorSentence semanticParser = SemparseAnnotatorSentence.fromSerializedModels("src/main/resources/parser.ser", "src/main/resources/supertagger.ser");
-		AnnotationVerb annotationVerb = new AnnotationVerb();
-
-		/*
-		 * Add micro-readers to the pipeline here
-		 */
-		addAnnotator(mentionCategorizer.produces(), mentionCategorizer);
-		addAnnotator(semanticParser.produces(), semanticParser);
-		addAnnotator(annotationVerb.produces(), annotationVerb);
-	}
->>>>>>> refs/remotes/origin/master
 }
+
+
