a96aada55257521845e89e2f7606f47469125444
Bill McDowell <forkunited@gmail.com>
2015-05-26 15:28:44 -0400

    Moved semantic parsing stuff to its own repository

--- src/main/java/edu/cmu/ml/rtw/micro/data/MicroDataTools.java
+++ src/main/java/edu/cmu/ml/rtw/micro/data/MicroDataTools.java
@@ -10,8 +10,8 @@ import edu.cmu.ml.rtw.generic.data.annotation.nlp.AnnotationTypeNLP;
 import edu.cmu.ml.rtw.generic.util.OutputWriter;
 import edu.cmu.ml.rtw.micro.cat.data.annotation.nlp.AnnotationTypeNLPCat;
 import edu.cmu.ml.rtw.micro.hdp.HDPParser;
-import edu.cmu.ml.rtw.micro.model.annotator.semparse.SemparseAnnotatorSentence;
 import edu.cmu.ml.rtw.ppa.predict.PPADisambiguator;
+import edu.cmu.ml.rtw.micro.sem.model.annotator.SemparseAnnotatorSentence;
 
 public class MicroDataTools extends DataTools {
 
@@ -25,7 +25,7 @@ public class MicroDataTools extends DataTools {
     /* Add micro-reading annotation types here */
     this.addAnnotationTypeNLP(AnnotationTypeNLPCat.NELL_CATEGORY);
     this.addAnnotationTypeNLP(AnnotationVerb.NELL_VERB);
-    this.addAnnotationTypeNLP(SemparseAnnotatorSentence.LOGICAL_FORM_ANNOTATION_TYPE);
+    this.addAnnotationTypeNLP(SemparseAnnotatorSentence.LOGICAL_FORM);
     this.addAnnotationTypeNLP(PPADisambiguator.PPA_DISAMBIG);
     this.addAnnotationTypeNLP(HDPParser.SEMANTIC_PARSE);
   }
@@ -38,7 +38,7 @@ public class MicroDataTools extends DataTools {
     List<AnnotationTypeNLP<?>> annotationTypes = new ArrayList<AnnotationTypeNLP<?>>();
     annotationTypes.add(AnnotationTypeNLPCat.NELL_CATEGORY);
     annotationTypes.add(AnnotationVerb.NELL_VERB);
-    annotationTypes.add(SemparseAnnotatorSentence.LOGICAL_FORM_ANNOTATION_TYPE);
+    annotationTypes.add(SemparseAnnotatorSentence.LOGICAL_FORM);
     annotationTypes.add(PPADisambiguator.PPA_DISAMBIG);
     return annotationTypes;
   }
--- src/main/java/edu/cmu/ml/rtw/micro/model/annotation/nlp/PipelineNLPMicro.java
+++ src/main/java/edu/cmu/ml/rtw/micro/model/annotation/nlp/PipelineNLPMicro.java
@@ -8,7 +8,7 @@ import edu.cmu.ml.rtw.micro.hdp.HDPParser;
 import edu.cmu.ml.rtw.micro.cat.data.CatDataTools;
 import edu.cmu.ml.rtw.micro.cat.data.annotation.CategoryList;
 import edu.cmu.ml.rtw.micro.cat.data.annotation.nlp.NELLMentionCategorizer;
-import edu.cmu.ml.rtw.micro.model.annotator.semparse.SemparseAnnotatorSentence;
+import edu.cmu.ml.rtw.micro.sem.model.annotator.SemparseAnnotatorSentence;
 import edu.cmu.ml.rtw.ppa.predict.PPADisambiguator;
 import edu.cmu.ml.rtw.AnnotationVerb;
 
